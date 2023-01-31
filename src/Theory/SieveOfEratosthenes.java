ccc216f8cfd81c1a10a487bd984aca7a75cbc8e8
Yagnesh <pancholiyagnesh2001@gmail.com>
2023-01-31 17:46:29 +0530

    first Commit

---
+++ src/Theory/SieveOfEratosthenes.java
@@ -0,0 +1,36 @@
+package Theory;
+
+public class SieveOfEratosthenes
+{
+	public static void main(String args[])
+	{
+		int n = 50;
+		boolean primes[] = new boolean[n + 1];
+		sieveOfEratosthenes(n, primes);
+	}
+	//bydefalut boolean array is false so we assume false means No is Prime
+
+	static void sieveOfEratosthenes(int n, boolean[] primes)
+	{
+		for(int i = 2; i * i <= n; i++)
+		{
+			if(!primes[i])
+			{
+				for(int j = i * 2; j <= n; j += i)
+				{
+					primes[j] = true;
+				}
+			}
+		}
+		for(int i = 2; i <= n; i++)
+		{
+			if(!primes[i])
+			{
+				System.out.println(i + " ");
+			}
+		}
+		{
+
+		}
+	}
+}
